97958133e4466c22f88a7d5cd9625f90da41c666
renoWNNNN <cicariboom@gmail.com>
2025-04-17 14:20:19 -0500

    Update ClientesController.cs

---
+++ homeworks/Final/SPAKLY.API/Controllers/ClientesController.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+using SPAKLY.Datos;
+using SPAKLY.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace SPAKLY.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientesController : ControllerBase
+    {
+        private readonly SPAKLYDbContext _context;
+
+        public ClientesController(SPAKLYDbContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cliente>>> GetClientes()
+        {
+            return await _context.Clientes.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Cliente>> GetCliente(int id)
+        {
+            var cliente = await _context.Clientes.FindAsync(id);
+            if (cliente == null)
+                return NotFound();
+
+            return cliente!;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Cliente>> PostCliente(Cliente cliente)
+        {
+            _context.Clientes.Add(cliente);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCliente), new { id = cliente.ClienteID }, cliente);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCliente(int id, Cliente cliente)
+        {
+            if (id != cliente.ClienteId)
+                return BadRequest();
+
+            _context.Entry(cliente).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
